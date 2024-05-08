72cb97a074ab7a25b607958fa33b65b139e8113d
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-05-08 14:55:28 +0530

    Created new project

---
+++ AFRICA/AFRICAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFRICAApp.swift
+//  AFRICA
+//
+//  Created by vinay Kumar ranjan on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AFRICAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFRICA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AFRICA
+//
+//  Created by vinay Kumar ranjan on 08/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
