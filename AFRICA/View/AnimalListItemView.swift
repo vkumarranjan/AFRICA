da3bb44f1001e9426ed9bd7fc4f6575145b009c7
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-05-16 07:29:33 +0530

    Adde Animal listItemView

---
+++ AFRICA/View/AnimalListItemView.swift
@@ -0,0 +1,61 @@
+//
+//  AnimalListItemView.swift
+//  AFRICA
+//
+//  Created by vinay Kumar ranjan on 15/05/24.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            Image(animal.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12)
+                )
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text(animal.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(animal.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+            } //: VSTACK
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalListItemView_Previews: PreviewProvider {
+  static let animals: [Animal] = Bundle.main.decode("animals.json")
+  
+  static var previews: some View {
+    AnimalListItemView(animal: animals[1])
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
+
+//let mock: [Animal] = Bundle.main.decode("animals.json")
+//
+//#Preview( traits: .sizeThatFitsLayout) {
+//    
+//    AnimalListItemView(animal: mock[1])
+//        .padding()
+//}
