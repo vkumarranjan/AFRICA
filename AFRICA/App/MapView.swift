4d071fd2be6b1d0258f36e0e51ade5f30c1f9875
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-06-26 18:03:40 +0530

    Added map view

--- AFRICA/App/MapView.swift
+++ AFRICA/App/MapView.swift
@@ -6,10 +6,88 @@
 //
 
 import SwiftUI
+import MapKit
+
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+      var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+      var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+      var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+      
+      return mapRegion
+    }()
+    
+    let location:[NationalParkLocation] = Bundle.main.decode("locations.json")
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // MARK: Basic map
+        //Map(coordinateRegion: $region)
+        // Map with anotation
+        
+        Map(coordinateRegion: $region, annotationItems: location, annotationContent: { item in
+           // MapPin(coordinate: item.location, tint: .accentColor)
+            
+            //MapMarker(coordinate: item.location, tint: .accentColor)
+            
+//            MapAnnotation(coordinate: item.location) {
+//                Image("logo")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 32, height: 32, alignment: .center)
+//            } // 
+            
+            MapAnnotation(coordinate: item.location) {
+                MapAnnotationView(location: item)
+            }
+        })
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            , alignment: .top
+        
+        )
+        
     }
 }
 
