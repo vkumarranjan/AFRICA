bd5761173ee3899fa8dbe4eed967b700bb9dcbf5
vinay Kumar ranjan <vinay.raj.apple@gmail.com>
2024-06-26 16:47:21 +0530

    Added content view

--- AFRICA/App/ContentView.swift
+++ AFRICA/App/ContentView.swift
@@ -8,16 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    @State var isGridViewActive = false
+    
     var body: some View {
         NavigationView {
-            List {
-                CoverImgageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImgageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
+                    } // List
+                    
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { item in
+                                NavigationLink(destination: AnimalDetailView(animal: item)){
+                                    AnimalGridItemView(animal: item)
+                                } //NavigationLink
+                            } // ForEach
+                        } // LazyVGrid
+                        .padding(10)
+                        .animation(.easeIn)
+                    }
+                }
             }
             .navigationBarTitle("Africa", displayMode: .large)
-        }
-    }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // List
+                        Button(action: {
+                            print("")
+                            isGridViewActive = false
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary  : .accentColor)
+                        }
+                        
+                        // Grid
+                        Button(action: {
+                            print("")
+                            isGridViewActive = true
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor  : .primary)
+
+                        }
+                    } // HStack
+                } // ToolbarItem
+            } // toolbar
+        } // NavigationView
+    } // body
 }
 
 #Preview {
